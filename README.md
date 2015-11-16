a6bf53a00645a8557d2fda57472e81f432e4b58e
Denny de la Haye <2013@denny.me>
2015-11-16 01:37:24 +0000

    Adding CNN parser.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Stale News Warning is a Chrome extension which warns you when you are reading 
 an old news article.
 
-Currently supported: BBC News, The Guardian, Huffington Post
+Currently supported: BBC News, CNN, The Guardian, Huffington Post
 
---
+++ cnn.js
@@ -0,0 +1,12 @@
+// (Current) BBC News date HTML:
+// <p class="date date--v1" data-seconds="1447545683"><strong>15 November 2015</strong>
+//   Last updated at 00:01 GMT </p>
+
+jQuery( function( $ ) {
+	// Extract the publication date from the page HTML
+	var published = $( 'p[class="update-time"]' ).text().replace( /^.*\)/i, '' );
+	
+	// Pass the date string to the date-checking function
+	dateCheck( published );
+} );
+
