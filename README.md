77086ab13cef5fed729ce517a0e362989b83fbaf
Denny de la Haye <2013@denny.me>
2015-11-16 00:43:30 +0000

    Adding support for the Huffington Post.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Stale News Warning is a Chrome extension which warns you when you are reading 
 an old news article.
 
+Currently supported: BBC News, The Guardian, Huffington Post
+
---
+++ huffingtonpost.js
@@ -0,0 +1,14 @@
+// (Current) Huffington Post date HTML:
+// <span class="posted">
+//     Posted: <time datetime="2015-11-15T13:57:53-05:00">
+//     15/11/2015 18:57 GMT        </time>
+// </span>
+
+jQuery( function( $ ) {
+	// Extract the publication date from the page HTML
+	var published = $( 'span[class="posted"]' ).find( 'time' ).attr( 'datetime' );
+	
+	// Pass the date string to the date-checking function
+	dateCheck( published );
+} );
+
