87e379a3f4546ba21574a36cbdcc7d206f9d351e
Denny de la Haye <2014@denny.me>
2015-11-16 19:16:01 +0000

    Adding parsers for Yahoo and Yahoo UK news sections

---
+++ yahoo-uk.js
@@ -0,0 +1,11 @@
+// (Current) Yahoo date HTML:
+// <abbr title="2012-08-23T10:58:41Z">Thu, Aug 23, 2012</abbr>
+
+jQuery( function( $ ) {
+	// Extract the publication date from the page HTML
+	var published = $( 'abbr' ).attr( 'title' );
+	
+	// Pass the date string to the date-checking function
+	dateCheck( published );
+} );
+
