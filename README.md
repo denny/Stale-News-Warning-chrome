49405fb4aedf7c530c56463d2688cdae3e5328f3
Denny de la Haye <2014@denny.me>
2015-11-16 20:00:59 +0000

    Adding parser for India Times

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Stale News Warning is a Chrome extension which warns you when you are reading
 an old news article.
 
 Currently supported: BBC News, CNN, Daily Mail, The Guardian, Huffington Post, 
-New York Times, Sky News (UK), Yahoo News, Yahoo News UK
+India Times, New York Times, Sky News (UK), Yahoo News, Yahoo News UK
 
 # Install
 https://chrome.google.com/webstore/detail/stale-news-warning/leimdfebooadifmdaofhjkplmpiimlpg
---
+++ indiatimes.js
@@ -0,0 +1,12 @@
+// India Times date HTML:
+// <div class="fl author_cont"><span class="fl"><img src="http://media.indiatimes.in/resources/images/indiatimes_author.jpg"/></span><div class="author_cont"><div class="author_fix"> By IndiaTimes</div><div>April 6, 2014</div></div></div></div></div>
+
+jQuery( function( $ ) {
+	// Extract the publication date from the page HTML
+	var published = $( 'div[class="fl author_cont"]' 
+		).find( 'div[class="author_cont"]' ).find( 'div:not([class])' ).text();
+	
+	// Pass the date string to the date-checking function
+	dateCheck( published );
+} );
+
