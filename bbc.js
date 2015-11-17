f17d95151cb73baa5066ec8abbea78349a84b920
Denny de la Haye <2014@denny.me>
2015-11-17 19:31:21 +0000

    Add older variation of date HTML to BBC parser

--- bbc.js
+++ bbc.js
@@ -1,10 +1,14 @@
 // (Current) BBC News date HTML:
-// <p class="date date--v1" data-seconds="1447545683"><strong>15 November 2015</strong>
-//   Last updated at 00:01 GMT </p>
+// <p class="date date--v1" data-seconds="1447545683"><strong>15 November 2015</strong> Last updated at 00:01 GMT </p>
+// OR: <div class="date date--v2" data-seconds="1352293816" data-datetime="7 November 2012">7 November 2012</div>
 
 jQuery( function( $ ) {
 	// Extract the publication date from the page HTML
-	var published = $( 'p[class="date date--v1"]' ).find( 'strong' ).html();
+	var published = '';
+	published = $( 'p[class="date date--v1"]' ).find( 'strong' ).text();
+	if ( published == '' ) {
+		published = $( 'div[class="date date--v2"]' ).attr( 'data-datetime');
+	}
 	
 	// Pass the date string to the date-checking function
 	dateCheck( published );
