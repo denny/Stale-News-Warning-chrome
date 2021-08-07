67d728954d42fc752e8da719a578484cf2f5558a
Paul Cochrane <paul.cochrane@posteo.de>
2021-08-07 12:24:43 +0200

    Re-enable stale news warnings for the India Times
    As with many other news sites, the publication date/time is stored in
    the `article:published_time` meta property.  However, sometimes this
    isn't seen by the stale news warning extension, hence the
    `datePublished` element of the page's JSON metadata (stored in a
    `<script>` element) is used as a fall-back.
    
    The weird thing with the India Times is that when loading a page the
    first time, the extension doesn't pick up any publication date/time
    information, however on reload it *does*.  Odd.

--- indiatimes.js
+++ indiatimes.js
@@ -1,10 +1,25 @@
 // India Times date HTML:
-// <div class="fl author_cont"><span class="fl"><img src="http://media.indiatimes.in/resources/images/indiatimes_author.jpg"/></span><div class="author_cont"><div class="author_fix"> By IndiaTimes</div><div>April 6, 2014</div></div></div></div></div>
+// <meta data-rh="true" property="article:published_time" content="Sat, 07 Aug 2021 10:40:41 GMT"/>
+// OR:
+// The date is embedded within the JSON metadata of a <script> element; the
+// object's key is called "datePublished"
 
 jQuery( function( $ ) {
 	// Extract the publication date from the page HTML
-	var published = $( 'div[class="fl author_cont"]' 
-		).find( 'div[class="author_cont"]' ).find( 'div:not([class])' ).text();
+        var published = '';
+	let published_time = $( 'meta[property="article:published_time"]' ).attr( 'content' );
+
+        if (published_time === undefined) {
+            let scripts = Array.from(document.querySelectorAll('script[type="application/ld+json"]'));
+            let scriptWithDatePublished = scripts.filter(
+                script => /datePublished/.test(script.innerText)
+            )[0];
+            let pageMetadta = JSON.parse(scriptWithDatePublished.innerText);
+            published = pageMetadta.datePublished;
+        }
+        else {
+            published = published_time;
+        }
 	
 	// Pass the date string to the date-checking function
 	dateCheck( published );
