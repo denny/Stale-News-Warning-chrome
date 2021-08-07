72d56ec816530c08f09154e5fe750c3527aa5587
Paul Cochrane <paul.cochrane@posteo.de>
2021-08-07 13:01:00 +0200

    Re-enable stale news warnings for The Telegraph
    ... which puts the publication date/time info in the `<meta>` tag with
    the name `DCSext.articleFirstPublished`.

--- telegraph.js
+++ telegraph.js
@@ -1,9 +1,9 @@
 // (Current) Telegraph date HTML:
-// <meta itemprop="datePublished" content="2016-01-24"/>
+// <meta name="DCSext.articleFirstPublished" content="2021-08-07 07:24"/>
 
 jQuery( function( $ ) {
 	// Extract the publication date from the page HTML
-	var published = $( 'meta[itemprop="datePublished"]' ).attr( 'content' );
+	var published = $( 'meta[name="DCSext.articleFirstPublished"]' ).attr( 'content' );
 	
     // Pass the date string to the date-checking function
 	dateCheck( published );
