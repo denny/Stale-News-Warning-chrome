19cdda95d36250a437e35e9b2e18b764913b8638
Paul Cochrane <paul.cochrane@posteo.de>
2021-08-07 13:06:53 +0200

    Extract publication date for Yahoo News
    ... which puts the publication date/time in the `datePublished` element
    of a JSON object embedded within a `<script>` element.
    
    Unfortunately, even though the publication date/time is extracted
    correctly, the extension can't show it because `alert()`s are forbidden
    on Yahoo News.

--- yahoo.js
+++ yahoo.js
@@ -1,9 +1,15 @@
 // (Current) Yahoo date HTML:
-// <abbr title="2012-08-23T10:58:41Z">Thu, Aug 23, 2012</abbr>
+// the date is embedded within the JSON metadata of a <script> element; the
+// object's key is called "datePublished"
 
 jQuery( function( $ ) {
 	// Extract the publication date from the page HTML
-	var published = $( 'abbr' ).text();
+        let scripts = Array.from(document.querySelectorAll('script[type="application/ld+json"]'));
+        let scriptWithDatePublished = scripts.filter(
+            script => /datePublished/.test(script.innerText)
+        )[0];
+        let pageMetadta = JSON.parse(scriptWithDatePublished.innerText);
+        let published = pageMetadta.datePublished;
 	
 	// Pass the date string to the date-checking function
 	dateCheck( published );
