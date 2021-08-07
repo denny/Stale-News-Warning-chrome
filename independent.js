c9b271ae4f8b4433e3c1e6e9fc84e0c57e853aa4
Paul Cochrane <paul.cochrane@posteo.de>
2021-08-07 12:21:26 +0200

    Re-enable stale news warnings for the Independent
    As with many other news sites, the publication date/time is stored in
    the `article:published_time` meta property.

--- independent.js
+++ independent.js
@@ -1,10 +1,9 @@
 // (Current) Independent date HTML:
-// <li title="16 November 2015 18:19 London"><time data-microtimes='{"published":"1447684200000","display":"000","changed":"1447697948000"}' datetime="18:19, 16 November 2015"unixtime="1447697948000">Monday 16 November 2015 18:19 BST</time></li>
-
+// <meta property="article:published_time" content="2021-08-07T08:06:23.000Z">
 
 jQuery( function( $ ) {
 	// Extract the publication date from the page HTML
-	var published = $('time').attr('datetime').split(',')[1];
+	var published = $( 'meta[property="article:published_time"]' ).attr( 'content' );
 	
 	// Pass the date string to the date-checking function
 	dateCheck( published );
