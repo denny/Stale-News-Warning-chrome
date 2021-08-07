f025150b52ea78eaa108ca360c009eab43986196
Paul Cochrane <paul.cochrane@posteo.de>
2021-08-07 12:29:36 +0200

    Re-enable stale news warnings for the NY Times
    As with many other news sites, the publication date/time is stored
    within the page's `article:published_time` meta property.

--- nytimes.js
+++ nytimes.js
@@ -1,16 +1,9 @@
 // New York Times date HTML:
-// <time class="dateline" datetime="2015-11-06" itemprop="datePublished" content="2015-11-06">NOV. 6, 2015</time>
-// OR: <div class="timestamp">Published: January 12, 2010 </div>
+// <meta data-rh="true" property="article:published_time" content="2021-08-07T09:00:26.000Z"/>
 
 jQuery( function( $ ) {
 	// Extract the publication date from the page HTML
-	var published = '';
-	published = $( 'time[itemprop="datePublished"]' ).attr( 'content' );
-	
-	if ( published == '' || !published ) {
-		published = $( 'div[class="timestamp"]' ).text();
-		published = published.replace( /Published: /, '' );
-	}
+	var published = $( 'meta[property="article:published_time"]' ).attr( 'content' );
 	
 	// Pass the date string to the date-checking function
 	dateCheck( published );
