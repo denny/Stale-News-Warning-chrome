752c8126016979bfc55c50fc4a804db1c8ee3796
Paul Cochrane <paul.cochrane@posteo.de>
2021-08-07 11:08:15 +0200

    Re-enable stale news warnings for the DailyMail
    The DailyMail uses what seems to have become a standard on news sites:
    the `<meta>` element with the `article:published_time` property contains
    the publication date/time data.

--- dailymail.js
+++ dailymail.js
@@ -1,9 +1,9 @@
 // (Current UK) Dailymail date HTML:
-// <meta itemprop="datePublished" content="2015-11-15T21:22:14+0000"/>
+// <meta property ="article:published_time" content="2021-08-07T00:43:23+0100" />
 
 jQuery( function( $ ) {
 	// Extract the publication date from the page HTML
-	var published = $( 'meta[itemprop="datePublished"]' ).attr( 'content' );
+	var published = $( 'meta[property="article:published_time"]' ).attr( 'content' );
 	
     // Pass the date string to the date-checking function
 	dateCheck( published );
