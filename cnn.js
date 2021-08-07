ff8a60e038a62c6dda21a4a344fcc06ce0dd3d54
Paul Cochrane <paul.cochrane@posteo.de>
2021-08-07 10:46:13 +0200

    Re-enable stale news warnings for CNN
    Although CNN uses what seems to have become a standard across many news
    sites for specifying the publication date/time (i.e. the `content`
    attributes of the `<meta>` element with the `"article:published_time"`
    property), this doesn't seem to be available when the extension is
    loaded.  However, CNN provides the `<meta>` element with the `pubdate`
    name and the date information is stored in this element's `content`
    attribute.  This change now gets the stale news warning to work again on
    CNN.

--- cnn.js
+++ cnn.js
@@ -1,19 +1,9 @@
 // CNN News date HTML:
-// <p class="update-time">Updated 1615 GMT (0015 HKT) November 5, 2015 <span id="js-pagetop_video_source" class="video__source top_source"></span></p>
-// OR: <div class="cnn_strytmstmp"><script type="text/javascript">if(location.hostname.indexOf( 'edition.' ) > -1) {document.write('January 13, 2010 -- Updated 1451 GMT (2251 HKT)');} else {document.write('January 13, 2010 9:51 a.m. EST');}</script></div>
+// <meta content="2021-08-07T00:32:40Z" name="pubdate">
 
 jQuery( function( $ ) {
 	// Extract the publication date from the page HTML
-	var published = '';
-	published = $( 'p[class="update-time"]' ).text();
-	published = published.replace( /^.*\)/i, '' );
-	published = published.replace( /\|.*$/i, '' );
-	
-	if ( published == '' ) {
-		published = $( 'div[class="cnn_strytmstmp"]' ).text();
-		published = published.replace( /^.*}/i, '' );
-		published = published.replace( /--.*$/i, '' );
-	}
+	var published = $( 'meta[name="pubdate"]' ).attr( 'content' );
 	
 	// Pass the date string to the date-checking function
 	dateCheck( published );
